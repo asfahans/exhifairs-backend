920badc882bf7ff64c98d8a064007d205fe5adb0
Asfahan Shaikh <asfahans@gmail.com>
2021-05-11 11:24:43 +0530

    Final Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a44117df33884eb4653f9c70f24f00f5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8256832b-a692-4bb5-b2eb-8256a00b3cd6'
+};
